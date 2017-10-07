8a28dd54e1e63e4680697aa42d268e909be22854
Sam Meech-Ward <sam@meech-ward.com>
2017-10-06 18:39:45 -0700

    Add a test to test sum(int fromN, int toN)

--- SumTests/SumTests.m
+++ SumTests/SumTests.m
@@ -14,26 +14,16 @@
 
 @implementation SumTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+int sum(int fromN, int toN) {
+    return 0;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)testSumFunction {
+    XCTAssertEqual(sum(1, 1), 1);
+    XCTAssertEqual(sum(1, 2), 3);
+    XCTAssertEqual(sum(3, 7), 25);
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
